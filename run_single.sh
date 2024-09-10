0624e1451465486755981217e250cf9644c0ef0b
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-09-10 13:27:49 +0200

    Okay, now the slurm stuff works.

--- run_single.sh
+++ run_single.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+echo "Experiment started."
+date
+
+source venv/bin/activate
+
 python benchmarl/run.py algorithm=mappo task=hierarchial/cruise_control  experiment.on_policy_collected_frames_per_batch=100 experiment.max_n_frames=1000000 experiment.evaluation_interval=10000 experiment.checkpoint_interval=10000 experiment.loggers=[csv] experiment.evaluation_episodes=1000 experiment.train_device=cuda experiment.buffer_device=cuda
+
+echo "Experiment ended"
+date
--- run_slurm.sh
+++ run_slurm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ARGS="--out=outputs/out.txt -e outputs.error.txt --partition=dhabi -n1 --mem=16G --job-name $job_name"
+
+ARGS="--out=outputs/out.txt -e outputs/error.txt --partition=turing -n1 --mem=16G --job-name benchmarl"
 
 sbatch $ARGS ./run_single.sh
