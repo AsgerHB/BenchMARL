3b402272b339042c714c22dd2f946a3cc378d37e
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-09-11 14:28:20 +0200

    Set results dir in run_single.sh

--- run_single.sh
+++ run_single.sh
@@ -5,7 +5,8 @@ date
 
 source venv/bin/activate
 
-python benchmarl/run.py algorithm=mappo task=hierarchial/cruise_control  experiment.on_policy_collected_frames_per_batch=100 experiment.max_n_frames=1000000 experiment.evaluation_interval=10000 experiment.checkpoint_interval=10000 experiment.loggers=[csv] experiment.evaluation_episodes=1000 experiment.train_device=cuda experiment.buffer_device=cuda
+results_dir="$HOMEDIR/Results/N-player CC MAPPO"
+python benchmarl/run.py algorithm=mappo task=hierarchial/cruise_control  experiment.on_policy_collected_frames_per_batch=100 experiment.max_n_frames=1000000 experiment.evaluation_interval=10000 experiment.checkpoint_interval=10000 experiment.loggers=[csv] experiment.evaluation_episodes=1000 experiment.train_device=cuda experiment.buffer_device=cuda experiment.save_folder=$results_dir
 
 echo "Experiment ended"
 date
