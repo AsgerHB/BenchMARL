b4404867f64dc63d72551b876945e970a2bb56e1
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-09-12 09:48:46 +0200

    Reapply "Change to more of a debug run"
    This reverts commit f512c1bd34ace985bc313fe80118cf059c892894.

--- run_single.sh
+++ run_single.sh
@@ -8,11 +8,11 @@ source venv/bin/activate
 results_dir="$HOME/Results/N-player\ CC\ MAPPO"
 python benchmarl/run.py algorithm=mappo task=hierarchial/cruise_control \
     experiment.on_policy_collected_frames_per_batch=1000 \
-    experiment.max_n_frames=1000000 \
-    experiment.evaluation_interval=10000 \
-    experiment.checkpoint_interval=10000 \
+    experiment.max_n_frames=20000 \
+    experiment.evaluation_interval=5000 \
+    experiment.checkpoint_interval=5000 \
     experiment.loggers="[csv]" \
-    experiment.evaluation_episodes=1000 \
+    experiment.evaluation_episodes=400 \
     experiment.train_device=cuda \
     experiment.buffer_device=cuda \
     experiment.save_folder="$results_dir" \
