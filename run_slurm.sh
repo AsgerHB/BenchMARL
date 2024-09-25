dc8cba012ee08dffa7509bb5390d99adc9b78cdb
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-09-25 11:16:26 +0200

    About to waste as much power as I have ever saved turning off lights when I leave a room

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,7 +2,7 @@
 
 ARGS="--out=outputs/out.txt -e outputs/error.txt --partition=turing -n1 --mem=16G --job-name benchmarl"
 
-repetitions=5
+repetitions=10
 for ((r=1; r<=$repetitions; r++)); do
     sbatch $ARGS ./run_single.sh
 done
