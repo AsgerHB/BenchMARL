1ca10c5cb6a0364905497a0483b8bda3f6ad8780
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-10-10 13:18:05 +0200

    Let's actually go with the low number of episodes, see if it works. Pilot run seems to suggest it would.

--- run_single.sh
+++ run_single.sh
@@ -12,8 +12,10 @@ python benchmarl/run.py algorithm=mappo task=hierarchial/chemical_production \
     experiment.train_device=cuda \
     experiment.buffer_device=cuda \
     experiment.save_folder="$results_dir" \
-    experiment.max_n_frames=6000000 \
-    experiment.evaluation_interval=600000 \
+    experiment.max_n_frames=200000 \
+    experiment.evaluation_interval=20000 \
+    experiment.on_policy_collected_frames_per_batch=300 \
+    experiment.on_policy_n_envs_per_worker=3 \
     task.safety_violation_penalty=1600 \
     seed=$RANDOM
 
