be0db67f6d38648554a6b2de05183b6e12731664
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-10-10 13:37:43 +0200

    Right, I didn't see any errors because I wasn't looking in the "error.txt" file

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ARGS="-e outputs/error.txt --partition=turing -n1 --mem=16G --job-name benchmarl"
+base_args="--partition=turing -n1 --mem=16G --job-name benchmarl"
 
 repetitions=10
 for ((r=1; r<=$repetitions; r++)); do
-    sbatch $ARGS "--out=outputs/out$r.txt" ./run_single.sh
+    sbatch $base_args "--out=outputs/out$r.txt -e outputs/error$r.txt" ./run_single.sh
 done
