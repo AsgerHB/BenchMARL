74b2f15549c844fdc957c30c7dc915209e840da8
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-10-10 09:50:17 +0200

    Wait only one repetition for the pilot run please and thank you

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,7 +2,7 @@
 
 ARGS="--out=outputs/out.txt -e outputs/error.txt --partition=turing -n1 --mem=16G --job-name benchmarl"
 
-repetitions=10
+repetitions=1
 for ((r=1; r<=$repetitions; r++)); do
     sbatch $ARGS ./run_single.sh
 done
