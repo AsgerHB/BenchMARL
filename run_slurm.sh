0d64beb1a64f7ccc0882057ace8fd6be85addc57
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-10-10 11:20:18 +0200

    Quality-of-life improvements to shellscripts

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ARGS="--out=outputs/out.txt -e outputs/error.txt --partition=turing -n1 --mem=16G --job-name benchmarl"
+ARGS="-e outputs/error.txt --partition=turing -n1 --mem=16G --job-name benchmarl"
 
 repetitions=10
 for ((r=1; r<=$repetitions; r++)); do
-    sbatch $ARGS ./run_single.sh
+    sbatch $ARGS "--out=outputs/out$r.txt" ./run_single.sh
 done
